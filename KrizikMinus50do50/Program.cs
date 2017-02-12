2f0ba4af304bd44e57ea210a21e0a8857d64b2d1
tonakriz <tonakriz@gmail.com>
2017-02-12 21:58:48 +0100

    First Commit

---
+++ KrizikMinus50do50/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minus50to50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pole = new int[101];
+            pole[0] = 0;
+            for (int i = 0; i < 101; i++)
+                pole[i] = i -50;
+            foreach (int i in pole)
+                Console.Write("{0} ", i);
+            Console.ReadKey();
+        }
+    }
+}

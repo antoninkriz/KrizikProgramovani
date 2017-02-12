dc3e9077e24b53378f561aeed0a5ef5bd3fcc7df
tonakriz <tonakriz@gmail.com>
2017-02-12 22:03:21 +0100

    First Commit

---
+++ KrizikCteniZapisDoSouboru/MainProgram.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cistpsat
+{
+    class MainProgram
+    {
+        public List<string> values = new List<string>();
+        public 
+        static void Main(string[] args)
+        {
+            MainProgram m = new MainProgram();
+            m.start();
+        }
+
+        public void start() {
+            Console.Clear();
+            Console.WriteLine("Pro čtení zvotle malé písmeno C, pro zápis malé Z");
+            String read = Console.ReadLine();
+
+            if (read.Equals("c"))
+            {
+                Read r = new Read();
+                r.readFile();
+                Console.ReadKey();
+                start();
+            }
+            else if (read.Equals("z"))
+            {
+                Write w = new Write();
+                w.writeFile();
+                Console.WriteLine("Zapsáno");
+                Console.ReadKey();
+                start();
+            }
+            else if (read.Equals(""))
+            {
+                Console.Clear();
+                Console.WriteLine("Restartuju");
+                Console.ReadKey();
+                start();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("ZKAZIL JSI TO!");
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Restartuju");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ KrizikCteniZapisDoSouboru/Write.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace cistpsat
+{
+    class Write
+    {
+        public void writeFile()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("Nyní napiš jméno");
+                String jmeno = Console.ReadLine();
+                Console.WriteLine("Nyní napiš příjmení");
+                String prijmeni = Console.ReadLine();
+                Console.WriteLine("Nyní napiš titul");
+                String titul = Console.ReadLine();
+
+                File.AppendAllText(@"jmeno.txt", Environment.NewLine + jmeno);
+                File.AppendAllText(@"prijmeni.txt", Environment.NewLine + prijmeni);
+                File.AppendAllText(@"titul.txt", Environment.NewLine + titul);
+                Console.WriteLine();
+            }
+            catch (Exception e) {
+                Console.WriteLine("Error writing to file: " + e);
+                Console.ReadKey();
+            }
+        }
+        
+	}
+}

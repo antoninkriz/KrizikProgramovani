a7310e85ec399a65cb2f3fae8b3d4778bdbd25b3
tona_kriz <tonakriz@gmail.com>
2018-01-02 20:42:38 +0100

    Update main.cpp
    Even shorter

--- KrizikBankomat/KrizikBankomat/main.cpp
+++ KrizikBankomat/KrizikBankomat/main.cpp
@@ -11,11 +11,9 @@ int main() {
         std::cin >> sCastka;
         if (sCastka.find_first_not_of(cisla) == std::string::npos && atoi(sCastka.c_str()) > 0 && atoi(sCastka.c_str()) % 200 == 0 && atoi(sCastka.c_str()) <= 24000) {
             iCastka = atoi(sCastka.c_str());
-            for (int x = 0; x < 4; x++) {
-                while (iCastka >= bankovky[x]) {
-                    iCastka -= bankovky[x];
-                    pocet[x]++;
-                }
+            for (int x = 0; x < 4; x++) while (iCastka >= bankovky[x]) {
+                iCastka -= bankovky[x];
+                pocet[x]++;
             }
             std::cout << "Uspesne vybrano " << sCastka << " ve formatu: " << pocet[0] << "x" << bankovky[0] << ", " << pocet[1] << "x" << bankovky[1] << ", " << pocet[2] << "x" << bankovky[2] << ", " << pocet[3] << "x" << bankovky[3] << endl << endl << pomlcky << endl << endl;
         } else {
